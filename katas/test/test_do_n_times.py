8de3f5aee8a06ce3aae2f1dac79b965d3b912bb5
ameertabri <ameer.t.2000@gmail.com>
2025-04-22 17:19:45 +0300

    kata

--- katas/test/test_do_n_times.py
+++ katas/test/test_do_n_times.py
@@ -4,4 +4,4 @@ from katas.do_n_times import say_hello
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(say_hello(), "Hello!")
+        self.assertEqual(say_hello(), say_hello())
